3e9c5d3d70dc0ae8f20400993a5ee19e7e02b22e
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-01-24 15:24:18 +0530

    Add files via upload

---
+++ opps3loading.java
@@ -0,0 +1,49 @@
+public class opps3loading {
+
+    final int x = 1;
+    public static void main(String[] args) {
+
+int x = 5;
+
+        System.out.println(x);
+        // System.out.println(4 + 3);
+        // System.out.println(4 + 3 +5);
+        // System.out.println(4 + 6.3);
+        // System.out.println(3.6f + 3.2);
+        // System.out.println("10" + 20);
+
+        // if(args.length == 7){
+        //     System.out.println("Done");
+        //    for(int i = 0 ; i<=6; i++){
+        //     args[i] = "2 " + i;
+        //    }
+        //    System.out.println(args[5]);
+        //    System.out.println(args[4]);
+        //    System.out.println(args[3]);
+           
+
+        //     return;
+        // }
+       
+        // System.out.println(args.length);
+        // main(new String[x++]);
+    
+    //     System.out.println(args);
+    //     System.out.println("main hua asli");
+        main();
+      main("7");
+      main(new int [5]);
+       
+       
+    }
+    public static void main(){
+System.out.println("Khali wala main hu");
+    }
+    public static void main(String x){
+        System.out.println(x);
+    }
+    public static void main(int arr []){
+System.out.println("int wale main hu ");
+    }
+}
+
